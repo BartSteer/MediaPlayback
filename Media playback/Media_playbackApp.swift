01af4bd75c93255d50bfcf2abb19752a56f4c212
bart <bart.steer@student.fontys.nl>
2024-09-10 09:56:08 +0200

    Initial Commit

---
+++ Media playback/Media_playbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Media_playbackApp.swift
+//  Media playback
+//
+//  Created by Bart Steer on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Media_playbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
